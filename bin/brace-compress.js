eef8b06dad4c3c067c30a1a8270b4cfa933cd712
Dave Eddy <dave@daveeddy.com>
2015-04-29 03:53:59 -0400

    add CLI tool

---
+++ bin/brace-compress.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/**
+ * simple CLI program to turn stdin into a brace-compressed string
+ *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ * Date: April 29, 2015
+ * License: MIT
+ */
+
+var fs = require('fs');
+
+var compress = require('../');
+
+var input = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+
+console.log(compress(input));
